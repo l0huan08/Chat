0153df0498cff377bd25d1e01a132b0a74385ea1
l0huan08@gmail.com <l0huan08@gmail.com>
2014-08-26 08:37:47 -0400

    finish MessageStation

--- Chat/src/Network/MessageStation.java
+++ Chat/src/Network/MessageStation.java
@@ -1,18 +1,20 @@
 package Network;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.net.SocketAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.util.Arrays;
 
 /**
  * The message sender/receiver.Use UDP protocol.
+ * Two stations A,B is connected, and A can send or receive message from station B
  * @author hl
  * @version 1.0
  * @created 25-°ËÔÂ-2014 13:32:38
@@ -21,57 +23,32 @@ public class MessageStation {
 
 	private static final int DATA_RECEIVE_BUF_SIZE = 256;
 	
-	private IP ip;
-	private IP listenIp; // the ip to listen
-	private DatagramSocket datagramSocket; // for UDP receiver
+	private IP ip; // own ip
+	private DatagramSocket receiverSocket; // for UDP receiver, connect to localhost:DEFAULT_PORT
+	private DatagramSocket senderSocket; // for UDP sender, connect to remoteIp:DEFAULT_PORT
 	private byte dataReceiveBuf[] = new byte[DATA_RECEIVE_BUF_SIZE];
 
 	public MessageStation() {
 		
 		// get local ip address
 		ip=null;
-		listenIp=null;
 		
 		try {
 			InetAddress ipAddr = InetAddress.getLocalHost();
 			String strIp = ipAddr.getHostAddress();
 			ip = IP.fromString(strIp);
-			listenIp = IP.fromString(strIp); // default is listen own ip
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		}
 		
 		try {
-			datagramSocket= new DatagramSocket(ip.getPort());
+			receiverSocket= new DatagramSocket(ip.getPort()); // recieverSocket is listen to A's own ip:port
+			senderSocket = new DatagramSocket(); // do not specify remote client
 		} catch (SocketException e) {
 			e.printStackTrace();
 		}
 	}
 
-	/**
-	 * Connect with a remote station
-	 * @param remoteIp
-	 * @return if success, return true
-	 */
-	public boolean connect(IP remoteIp) {
-		if (remoteIp==null)
-			return false;
-		
-		this.listenIp = remoteIp;
-		int[] addr = listenIp.getAddress();
-		byte[] addrByte = new byte[4];
-		for (int i=0;i<4;i++) {
-			addrByte[i] = (byte)(addr[i]);
-		}
-		try {
-			datagramSocket.connect( InetAddress.getByAddress(addrByte), listenIp.getPort());
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-			return false;
-		}
-		
-		return true;
-	}
 	
 	/**
 	 * Send a message. Return success.
@@ -80,23 +57,36 @@ public class MessageStation {
 	 * @param message
 	 */
 	public boolean send(IP dstIp, Message message){
-		return false;
+		// create data bytes from message object
+		ObjectOutputStream obs = null;
+		ByteArrayOutputStream bs = new ByteArrayOutputStream();
+		try {
+			obs = new ObjectOutputStream(bs);
+			obs.writeObject(message);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}	
+		byte data[] = bs.toByteArray();  // Converts Message to byte[].
+		DatagramPacket dp = new DatagramPacket(data, data.length,  ip2inet(dstIp), dstIp.getPort());
+		
+		try {
+			senderSocket.send(dp);// If client is not already waiting, data is lost.
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+		
+		return true;
 	}
 
 	/**
 	 * Waiting and receive a message. Blocking until a message received.
 	 */
 	public Message receive(){
-		if ( !datagramSocket.isConnected() ) {
-			boolean suc = connect(this.listenIp);
-			if (!suc)
-				return null;
-		}
-		
 		// receive a packet
 		DatagramPacket dataPacket = new DatagramPacket(dataReceiveBuf,DATA_RECEIVE_BUF_SIZE); // Can receive a max of 7 bytes
 		try {
-			datagramSocket.receive(dataPacket);
+			receiverSocket.receive(dataPacket);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -119,4 +109,20 @@ public class MessageStation {
 		return msg;
 	}
 
+	
+	private InetAddress ip2inet(IP ip) {
+		int[] addr = ip.getAddress();
+		byte[] addrByte = new byte[4];
+		for (int i=0;i<4;i++) {
+			addrByte[i] = (byte)(addr[i]);
+		}
+		try {
+			return InetAddress.getByAddress(addrByte);
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
 }
